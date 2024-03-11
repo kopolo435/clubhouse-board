00e555078b5941eafffa57979707f0291dee8b88
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-11 13:27:20 -0500

    Adds fields to user models

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
   img_url: { type: String },
