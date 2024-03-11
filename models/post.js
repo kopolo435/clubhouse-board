a2296292ebadbb77d14296bb868cef47a1b91d0b
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-11 09:05:01 -0500

    Adds Post database model

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true, maxLength: 30 },
+  content: { type: String, required: true },
+  img_url: { type: String },
+  owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  points: { type: Number, default: 1 },
+  date: { type: Date, default: Date.now },
+});
+
+PostSchema.virtual("formatted_date").get(function () {
+  return DateTime.fromJSDate(this.date, { zone: "utc" }).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+
+module.exports = mongoose.model("Post", PostSchema);
