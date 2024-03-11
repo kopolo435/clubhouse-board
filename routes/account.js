5c59d6594752b2b1873316b2636aca7c4480acb8
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-11 11:23:02 -0500

    Renombra archivo

--- routes/account.js
+++ routes/account.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const accountController = require("../controllers/account");
+const accountController = require("../controllers/accountController");
 
 // GET Sign-in
 router.get("/sign-in", accountController.sign_in_get);
