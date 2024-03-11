6a21ebe6ee88985e4f5b2d5823db58106afc5bbc
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-11 10:39:42 -0500

    Adds is_member field to user model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   img_url: { type: String },
+  is_member: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("User", UserSchema);
