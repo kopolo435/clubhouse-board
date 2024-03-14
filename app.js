51caca4b4cd6fdbd906d033586a58478e2caafc6
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-03-14 09:39:05 -0500

    Adds middleware to ease access to user data

--- app.js
+++ app.js
@@ -78,6 +78,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
